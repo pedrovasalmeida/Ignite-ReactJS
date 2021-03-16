afa28bf411d9f8415f58e60e441c53461567eced
Pedro Vasconcellos <pedrovasalmeida@gmail.com>
2021-03-16 17:47:25 -0300

    adding modal

--- 02-dtmoney/src/App.tsx
+++ 02-dtmoney/src/App.tsx
@@ -1,13 +1,37 @@
+import { useState } from 'react';
+
+import Modal from 'react-modal';
+
 import { Header } from './components/Header';
 import { Dashboard } from './components/Dashboard';
 
 import { GlobalStyle } from './styles/global';
 
+Modal.setAppElement('root');
+
 export function App() {
+  const [isNewTransactionModalOpen, setIsNewTransactionModalOpen] = useState(
+    false
+  );
+
+  function handleOpenNewTransactionModalOpen() {
+    setIsNewTransactionModalOpen(true);
+  }
+
+  function handleCloseNewTransactionModalOpen() {
+    setIsNewTransactionModalOpen(false);
+  }
+
   return (
     <>
-      <Header />
+      <Header onNewTransactionModalOpen={handleOpenNewTransactionModalOpen} />
       <Dashboard />
+      <Modal
+        isOpen={isNewTransactionModalOpen}
+        onRequestClose={handleCloseNewTransactionModalOpen}
+      >
+        <h2>Teste</h2>
+      </Modal>
       <GlobalStyle />
     </>
   );
