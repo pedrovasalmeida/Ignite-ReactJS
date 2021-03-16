f61147dac05979e436b697a5fd0d612b26717528
Pedro Vasconcellos <pedrovasalmeida@gmail.com>
2021-03-16 19:25:23 -0300

    adding some miragejs settings

--- 02-dtmoney/src/index.tsx
+++ 02-dtmoney/src/index.tsx
@@ -1,23 +1,55 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { createServer } from 'miragejs';
+import { createServer, Model } from 'miragejs';
 import { App } from './App';
 
 createServer({
+  models: {
+    transaction: Model,
+  },
+
+  seeds(server) {
+    server.db.loadData({
+      transactions: [
+        {
+          id: 1,
+          title: 'Freelance de website',
+          type: 'deposit',
+          category: 'Dev',
+          value: 2000,
+          createdAt: new Date('2021-02-12 12:00:00'),
+        },
+        {
+          id: 2,
+          title: 'Freelance de website',
+          type: 'deposit',
+          category: 'Dev',
+          value: 1000,
+          createdAt: new Date('2021-02-15 09:00:00'),
+        },
+        {
+          id: 3,
+          title: 'Pizza + Refri',
+          type: 'withdraw',
+          category: 'Comida',
+          value: 70,
+          createdAt: new Date('2021-02-16 19:00:00'),
+        },
+      ],
+    });
+  },
+
   routes() {
     this.namespace = 'api';
 
     this.get('/transactions', () => {
-      return [
-        {
-          id: 1,
-          title: 'Transaction 1',
-          value: 400,
-          type: 'deposit',
-          category: 'Food',
-          createdAt: new Date(),
-        },
-      ];
+      return this.schema.all('transaction');
+    });
+
+    this.post('/transactions', (schema, request) => {
+      const data = JSON.parse(request.requestBody);
+
+      return schema.create('transaction', data);
     });
   },
 });
