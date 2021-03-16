0df39dd3cd1379f42df250f3531145635d312a64
Pedro Vasconcellos <pedrovasalmeida@gmail.com>
2021-03-16 18:14:27 -0300

    adding global modal styles

--- 02-dtmoney/src/styles/global.ts
+++ 02-dtmoney/src/styles/global.ts
@@ -52,4 +52,46 @@ export const GlobalStyle = createGlobalStyle`
     opacity: 0.6;
     cursor: not-allowed;
   }
+
+  .react-modal-overlay {
+    background: rgba(0, 0, 0, 0.7);
+    
+    position: fixed;
+    top: 0;
+    bottom: 0;
+    right: 0;
+    left: 0;
+
+    display: flex;
+    align-items: center;
+    justify-content: center;
+  }
+
+  .react-modal-content {
+    width: 100%;
+    max-width: 576px;
+
+    border-radius: 0.25rem;
+
+    background: var(--background);
+    padding: 3rem;
+    outline: 0;
+
+    position: relative;
+  }
+
+  .react-modal-close {
+    position: absolute;
+    right: 1.5rem;
+    top: 1.5rem;
+
+    border: 0;
+    background: transparent;
+
+    transition: filter 200ms ease;
+    
+    &:hover {
+      filter: brightness(0.8);
+    }
+  }
 `;
