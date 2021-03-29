bf59c0ffa1d1fc8f0e0474fa4d1f38507a63d413
Pedro Vasconcellos <pedrovasalmeida@gmail.com>
2021-03-28 21:18:54 -0300

    changing ssr to ssg

--- 03-ignews/src/pages/index.tsx
+++ 03-ignews/src/pages/index.tsx
@@ -1,4 +1,4 @@
-import { GetServerSideProps } from 'next';
+import { GetStaticProps } from 'next';
 import Head from 'next/head';
 import SubscribeButton from '../components/SubscribeButton';
 import { stripe } from '../services/stripe';
@@ -41,7 +41,7 @@ export default function Home({ product }: HomeProps) {
   );
 }
 
-export const getServerSideProps: GetServerSideProps = async () => {
+export const getStaticProps: GetStaticProps = async () => {
   const price = await stripe.prices.retrieve('price_1Ia888DGO1mIoqcfmfs5942Y');
 
   const product = {
@@ -56,5 +56,6 @@ export const getServerSideProps: GetServerSideProps = async () => {
     props: {
       product,
     },
+    revalidate: 60 * 60 * 24, // 24 hours
   };
 };
