feecd78abcefc544d3654d91e8fc98ff4bba96dc
Pedro Vasconcellos <pedrovasalmeida@gmail.com>
2021-04-05 19:18:49 -0300

    creating posts exibition logic and pagination

--- desafio03-criando-um-projeto-do-zero/src/pages/index.tsx
+++ desafio03-criando-um-projeto-do-zero/src/pages/index.tsx
@@ -1,5 +1,14 @@
-import { GetStaticProps } from 'next';
+import { useState } from 'react';
 
+import { GetStaticProps } from 'next';
+import Head from 'next/head';
+import Link from 'next/link';
+import { IoMdCalendar } from 'react-icons/io';
+import { AiOutlineUser } from 'react-icons/ai';
+
+import Prismic from '@prismicio/client';
+
+import { RichText } from 'prismic-dom';
 import { getPrismicClient } from '../services/prismic';
 
 import commonStyles from '../styles/common.module.scss';
@@ -24,13 +33,112 @@ interface HomeProps {
   postsPagination: PostPagination;
 }
 
-// export default function Home() {
-//   // TODO
-// }
+export default function Home({ postsPagination }: HomeProps) {
+  const [nextPage, setNextPage] = useState(postsPagination.next_page);
+  const [postsData, setPostsData] = useState(postsPagination.results);
 
-// export const getStaticProps = async () => {
-//   // const prismic = getPrismicClient();
-//   // const postsResponse = await prismic.query(TODO);
+  async function handleLoadNextPage() {
+    const response = await fetch(nextPage);
 
-//   // TODO
-// };
+    if (response.status !== 200) {
+      return;
+    }
+
+    const posts = await response.json();
+
+    const updatedPosts = posts.results.map(post => {
+      return {
+        uid: post.uid,
+        first_publication_date: post.first_publication_date
+          ? new Date(post.first_publication_date).toLocaleDateString('pt-BR', {
+              day: '2-digit',
+              month: 'short',
+              year: 'numeric',
+            })
+          : null,
+        data: {
+          title: post.data.title,
+          subtitle: post.data.subtitle,
+          author: post.data.author,
+        },
+      };
+    });
+
+    setPostsData(prevState => [...prevState, ...updatedPosts]);
+    setNextPage(posts.next_page);
+  }
+
+  return (
+    <>
+      <Head>
+        <title>Home | spacetraveling</title>
+      </Head>
+
+      <main className={styles.container}>
+        <div className={styles.posts}>
+          {postsData.map(post => (
+            <Link key={post.uid} href={`/post/${post.uid}`}>
+              <a>
+                <strong>{post.data.title}</strong>
+                <p>{post.data.subtitle}</p>
+                <div>
+                  <time>
+                    <IoMdCalendar size={20} /> {post.first_publication_date}
+                  </time>
+                  <span>
+                    <AiOutlineUser size={20} /> {post.data.author}
+                  </span>
+                </div>
+              </a>
+            </Link>
+          ))}
+
+          {nextPage && (
+            <button type="button" onClick={handleLoadNextPage}>
+              Carregar mais posts
+            </button>
+          )}
+        </div>
+      </main>
+    </>
+  );
+}
+
+export const getStaticProps = async () => {
+  const prismic = getPrismicClient();
+  const postsResponse = await prismic.query(
+    [Prismic.predicates.at('document.type', 'post-challenge')],
+    {
+      pageSize: 1,
+    }
+  );
+
+  const posts = postsResponse.results.map(post => {
+    return {
+      uid: post.uid,
+      first_publication_date: post.first_publication_date
+        ? new Date(post.first_publication_date).toLocaleDateString('pt-BR', {
+            day: '2-digit',
+            month: 'short',
+            year: 'numeric',
+          })
+        : null,
+      data: {
+        title: post.data.title,
+        subtitle: post.data.subtitle,
+        author: post.data.author,
+      },
+    };
+  });
+
+  const postsPagination = {
+    next_page: postsResponse.next_page,
+    results: posts,
+  };
+
+  return {
+    props: {
+      postsPagination,
+    },
+  };
+};
