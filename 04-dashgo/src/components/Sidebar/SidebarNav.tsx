835bba5b86c917b3e5034116ae5177565548bec6
Pedro Vasconcellos <pedrovasalmeida@gmail.com>
2021-04-13 17:32:00 -0300

    adding navigation and visual response when navigate

--- 04-dashgo/src/components/Sidebar/SidebarNav.tsx
+++ 04-dashgo/src/components/Sidebar/SidebarNav.tsx
@@ -13,12 +13,20 @@ export function SidebarNav() {
   return (
     <Stack spacing="12" align="flex-start">
       <NavSection title="Geral">
-        <NavLink icon={RiDashboardLine}>Dashboard</NavLink>
-        <NavLink icon={RiContactsLine}>Usuários</NavLink>
+        <NavLink href="/dashboard" icon={RiDashboardLine}>
+          Dashboard
+        </NavLink>
+        <NavLink href="/users" icon={RiContactsLine}>
+          Usuários
+        </NavLink>
       </NavSection>
       <NavSection title="Automação">
-        <NavLink icon={RiInputMethodLine}>Formulário</NavLink>
-        <NavLink icon={RiGitMergeLine}>Automação</NavLink>
+        <NavLink href="/forms" icon={RiInputMethodLine}>
+          Formulário
+        </NavLink>
+        <NavLink href="/automation" icon={RiGitMergeLine}>
+          Automação
+        </NavLink>
       </NavSection>
     </Stack>
   );
