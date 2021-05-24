bdbb76c3d624cb05e5eb46ebd4d39fc1f2ff2935
Pedro Henrique <pedro.henrique@clippingcacd.com.br>
2021-05-24 08:01:02 -0300

    feat: :sparkles: adding permissions handler

---
+++ 05-auth/utils/validateUserPermissions.ts
@@ -0,0 +1,36 @@
+type User = {
+  permissions: string[];
+  roles: string[];
+};
+
+type ValidateUserPermissionsParams = {
+  user: User;
+  permissions?: string[];
+  roles?: string[];
+};
+
+export function validateUserPermissions({
+  user,
+  permissions,
+  roles,
+}: ValidateUserPermissionsParams) {
+  if (permissions?.length > 0) {
+    const hasAllPermissions = permissions.every((permissions) =>
+      user.permissions.includes(permissions)
+    );
+
+    if (!hasAllPermissions) {
+      return false;
+    }
+  }
+
+  if (roles?.length > 0) {
+    const hasAllRoles = roles.some((roles) => user.roles.includes(roles));
+
+    if (!hasAllRoles) {
+      return false;
+    }
+  }
+
+  return true;
+}
